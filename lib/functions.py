441c0e3954fde243525bf704a51f85972a7de62c
Alex Pearce <alex@alexpearce.me>
2012-04-30 10:59:00 +0100

    Add a couple of library functions.

---
+++ lib/functions.py
@@ -0,0 +1,6 @@
+""" Returns -1, 0, +1 for x < =, x == 0 and x > 0 respectively """
+sgn = lambda x : cmp(x,0)
+
+def sign(a, b):
+  """Returns |a| if b >= 0, -|a| otherwise"""
+  return fabs(a) if b >= 0 else -fabs(a)
